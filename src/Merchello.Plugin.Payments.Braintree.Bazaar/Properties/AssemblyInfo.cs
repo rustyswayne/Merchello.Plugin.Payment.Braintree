90165e05801ef3e95460be7caf85e778763bf3a3
Rusty Swayne <rs@merchello.com>
2015-12-15 10:57:42 -0800

    updates NuGet dependencies and patches build script

--- src/Merchello.Plugin.Payments.Braintree.Bazaar/Properties/AssemblyInfo.cs
+++ src/Merchello.Plugin.Payments.Braintree.Bazaar/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.3.*")]
-[assembly: AssemblyInformationalVersion("2.3.0-alpha-000004")]
+[assembly: AssemblyInformationalVersion("2.3.0-alpha-000006")]
 
 
